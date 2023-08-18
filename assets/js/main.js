142146e3102b8c0b1b5fb9e17e66e3a870d89c3d
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2023-08-17 22:10:57 -0300

    Feat: add API EmailJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,32 @@ const shadowHeader = () =>{
                        : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
+
+// Email JS
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_abkrlhe','template_fds6d3o','#contact-form', 'zqlM144fwWOYUhBGn')
+    .then(() => {
+        //Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+
+    }, () => {
+        // Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
